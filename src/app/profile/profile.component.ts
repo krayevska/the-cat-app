9fa64701497c7016039f1f709e1ba320059d9b72
Olga Krayevska <olha.krayevska@gmail.com>
2022-06-30 23:38:26 +0300

    profile refactoring

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,8 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Cat } from '../model/interfaces';
 
+const CAT_KEY = 'theCat';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -18,55 +20,36 @@ export class ProfileComponent implements OnInit {
     this.catsState$ = store.select('catsState');
   }
 
-  public catId: string;
   public cats: Cat[];
   catsState$: Observable<Cat[]>;
-  public catImgUrl: string;
-  public breedsData: any;
-  public breeds: string[] = [];
-  cat: Cat;
+  public cat: Cat;
 
   ngOnInit(): void {
-    if (localStorage.getItem('theCat')) {
-      this.route.params.subscribe((params: Params) => {
-        this.catId = params['id'];
-        const cat = JSON.parse(localStorage.getItem('theCat'));
-        if (cat.id === this.catId) {
-          this.cat = cat;
-        } else {
-          this.getNewCatData(this.catId);
-        }
-      });
-    } else {
-      this.route.params.subscribe((params: Params) => {
-        this.catId = params['id'];
-        this.getNewCatData(this.catId);
-      });
-    }
-  }
-
-  public getNewCatData(id: string): void {
-    this.catsState$.subscribe((data: Cat[] | []) => {
-      this.cats = data;
-      this.cat =
-        this.cats.length > 1
-          ? this.cats.find((cat) => cat.id === id)
-          : this.cats[0];
-      this.setCatToLocalStorage(this.cat);
+    const catFromStorage = JSON.parse(localStorage.getItem(CAT_KEY));
+    this.route.params.subscribe((params: Params) => {
+      const catParamsId = params['id'];
+      if (catFromStorage?.id === catParamsId) {
+        this.cat = catFromStorage;
+      } else {
+        this.getNewCatData(catParamsId);
+      }
     });
   }
 
-  public doesItHaveBreed(): boolean {
-    return this.cat.breeds && this.cat.breeds.length > 0;
+  public getNewCatData(id: string): void {
+    this.catsState$.subscribe((cats: Cat[]) => {
+      this.cat = cats.length > 1 ? cats.find((cat) => cat.id === id) : cats[0];
+      if (this.cat) {
+        localStorage.setItem(CAT_KEY, JSON.stringify(this.cat));
+      }
+    });
+  }
+
+  public hasBreed(): boolean {
+    return !!this.cat?.breeds?.length;
   }
 
   public goBack(): void {
     this.router.navigateByUrl('');
-  }
-
-  public setCatToLocalStorage(cat: Cat): void {
-    if (cat) {
-      localStorage.setItem('theCat', JSON.stringify(cat));
-    }
   }
 }
